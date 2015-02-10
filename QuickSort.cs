6f3bc40aab80ff5adc413faee30443168aaa0b62
JelenaBarinova <fiodorova.jelena@gmail.com>
2015-02-10 11:53:21 -0800

    Tests were cleaned-up

--- QuickSort.cs
+++ QuickSort.cs
@@ -4,36 +4,42 @@ public static class QuickS
 {
   public static void RunQuickSort()
   {
+    bool result;
+
     Console.WriteLine("QuickSort");
     Console.WriteLine("1. Random numbers:");
-    int[] arr = {8, 5, 10, 7, 3, 9};
-    int[] arrSorted = QuickSort(arr);
-    foreach (int i in arrSorted)
-    Console.Write(i.ToString() + " ");
-    Console.WriteLine();
+    result = TestQuickSort(new int[]{8, 5, 9, 10, 2, 1, 7});
+    Console.WriteLine(result?"OK":"Failed");
 
     Console.WriteLine("2. Array containing one item:");
-    int[] arr2 = {650};
-    int[] arr2Sorted = QuickSort(arr2);
-    foreach (int i in arr2Sorted)
-    Console.Write(i.ToString() + " ");
-    Console.WriteLine();
+    result = TestQuickSort(new int[]{650});
+    Console.WriteLine(result?"OK":"Failed");
 
     Console.WriteLine("3. Empty array:");
-    int[] arr3 = {};
-    int[] arr3Sorted = QuickSort(arr3);
-    foreach (int i in arr3Sorted)
-    Console.Write(i.ToString() + " ");
-    Console.WriteLine();
+    result = TestQuickSort(new int[]{});
+    Console.WriteLine(result?"OK":"Failed");
+
 
     Console.WriteLine("4. Two items array:");
-    int[] arr4 = {8, 6};
-    int[] arr4Sorted = QuickSort(arr4);
-    foreach (int i in arr4Sorted)
-    Console.Write(i.ToString() + " ");
-    Console.WriteLine();
-  }
+    result = TestQuickSort(new int[]{6, 5});
+    Console.WriteLine(result?"OK":"Failed");
 
+  }
+  public static bool TestQuickSort(int[] arr)
+  {
+    int[] arrSorted = QuickSort(arr);
+    Array.Sort(arr);
+    int i = 0;
+    while (i < arr.Length)
+    {
+      if (arr[i] != arrSorted[i])
+      {
+        return false;
+      }
+      i++;
+    }
+    return true;
+  }
   public static int[] QuickSort(int[] arr)
   {
     int n = arr.Length;
